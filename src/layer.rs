840cccc5bdffa77c4c850df15280a6b09bccdc9d
单子亨 <2366910832@qq.com>
2025-02-22 02:13:43 +0000

    Added a parameter to update_parameters

--- src/layer.rs
+++ src/layer.rs
@@ -1,9 +1,9 @@
-use crate::Numbers;
+use crate::{Number, Numbers};
 
 pub trait Layer {
     fn forward(&self, x: Numbers) -> Numbers;
 
     fn backward(&self, grad_z: Numbers) -> Numbers;
 
-    fn update_parameters(&mut self) {}
+    fn update_parameters(&mut self, _learning_rate: Number) {}
 }
