2c3d17580dda478dfb8b89141efe5613f17e1dd3
单子亨 <2366910832@qq.com>
2025-02-22 18:00:28 +0000

    Reimplemented the activation functions

--- src/activation_functions.rs
+++ src/activation_functions.rs
@@ -3,18 +3,22 @@ use crate::{
     utils::{relu_derivative_single, relu_single, sigmoid_derivative_single, sigmoid_single},
 };
 
-pub fn sigmoid(x: Vector) -> Vector {
-    x.into_iter().map(sigmoid_single).collect()
+pub fn sigmoid(mut x: Vector) -> Vector {
+    x.par_mapv_inplace(sigmoid_single);
+    x
 }
 
-pub fn sigmoid_derivative(x: Vector) -> Vector {
-    x.into_iter().map(sigmoid_derivative_single).collect()
+pub fn sigmoid_derivative(mut x: Vector) -> Vector {
+    x.par_mapv_inplace(sigmoid_derivative_single);
+    x
 }
 
-pub fn relu(x: Vector) -> Vector {
-    x.into_iter().map(relu_single).collect()
+pub fn relu(mut x: Vector) -> Vector {
+    x.par_mapv_inplace(relu_single);
+    x
 }
 
-pub fn relu_derivative(x: Vector) -> Vector {
-    x.into_iter().map(relu_derivative_single).collect()
+pub fn relu_derivative(mut x: Vector) -> Vector {
+    x.par_mapv_inplace(relu_derivative_single);
+    x
 }
