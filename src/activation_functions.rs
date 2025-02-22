76f6ce84d97c3f5f4567cf450df09957ec7e1bed
单子亨 <2366910832@qq.com>
2025-02-22 02:12:28 +0000

    Separated single number functions from activation_functions

--- src/activation_functions.rs
+++ src/activation_functions.rs
@@ -1,25 +1,7 @@
-use crate::{Number, Numbers};
-
-fn sigmoid_single(x: Number) -> Number {
-    1.0 as Number / (1.0 as Number + (-x).exp())
-}
-
-fn sigmoid_derivative_single(x: Number) -> Number {
-    let s = sigmoid_single(x);
-    s * (1.0 as Number - s)
-}
-
-fn relu_single(x: Number) -> Number {
-    if x > 0.0 as Number { x } else { 0.0 as Number }
-}
-
-fn relu_derivative_single(x: Number) -> Number {
-    if x > 0.0 as Number {
-        1.0 as Number
-    } else {
-        0.0 as Number
-    }
-}
+use crate::{
+    Numbers,
+    utils::{relu_derivative_single, relu_single, sigmoid_derivative_single, sigmoid_single},
+};
 
 pub fn sigmoid(x: Numbers) -> Numbers {
     x.into_iter().map(sigmoid_single).collect()
