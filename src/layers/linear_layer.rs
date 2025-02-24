773025479458ae8327b68164d99029e7a97a6994
单子亨 <2366910832@qq.com>
2025-02-24 12:33:16 +0000

    Implemented new() for LinearLayer

--- src/layers/linear_layer.rs
+++ src/layers/linear_layer.rs
@@ -1,20 +1,37 @@
-use crate::{Matrix, Number, Vector};
+use crate::{
+    Matrix, Number, Vector,
+    utils::{repeat, xavier_init, xavier_init_matrix},
+};
 
 use super::layer::Layer;
 
 pub struct LinearLayer {
-    _input_size: usize,
-    _output_size: usize,
-    _weights: Matrix,
-    _biases: Matrix,
-    _w_cache: Cell<Matrix>,
-    _b_cache: Cell<Matrix>,
-    _input_cache: Cell<Vector>,
+    input_size: usize,
+    output_size: usize,
+    weights: Matrix,
+    bias: Vector,
+    grad_w_cache: Option<Matrix>,
+    grad_b_cache: Option<Matrix>,
+    input_cache: Option<Matrix>,
 }
 
 impl LinearLayer {
-    pub fn _new(_input_size: usize, _output_size: usize, _gain: Option<Number>) -> Self {
-        unimplemented!()
+    pub fn new(input_size: usize, output_size: usize, gain: Option<Number>) -> Self {
+        let gain = if let Some(gain) = gain {
+            gain
+        } else {
+            1.0 as Number
+        };
+
+        Self {
+            input_size,
+            output_size,
+            weights: xavier_init_matrix((input_size, output_size), gain),
+            bias: xavier_init(output_size, gain),
+            grad_w_cache: None,
+            grad_b_cache: None,
+            input_cache: None,
+        }
     }
 }
 
