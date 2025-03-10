6193d25696e1a6f9430f19952c5f3c213a734de8
单子亨 <2366910832@qq.com>
2025-03-10 03:45:38 +0000

    Implemented forward and backward for CrossEntropyLossLayer

--- src/layers/cross_entropy_loss_layer.rs
+++ src/layers/cross_entropy_loss_layer.rs
@@ -1,17 +1,31 @@
-use crate::{Number, Vector};
+use crate::{Number, Vector, activation_functions::softmax};
 
 use super::loss_layer::LossLayer;
 
+/// Computes the softmax followed by the negative log-likelihood loss.
 pub struct CrossEntropyLossLayer {
-    target_prob_cache: Option<(Vector, Vector)>,
+    prob_target_cache: Option<(Vector, Vector)>,
 }
 
 impl LossLayer for CrossEntropyLossLayer {
     fn forward(&mut self, pred: Vector, target: Vector) -> Number {
-        unimplemented!()
+        let n_obs = target.len();
+        let prob = softmax(pred);
+        let result =
+            -1.0 as Number / n_obs as Number * (&target * prob.mapv(Number::ln).to_owned()).sum();
+
+        self.prob_target_cache = Some((prob, target));
+
+        result
     }
 
     fn backward(&self) -> Vector {
-        unimplemented!()
+        let (prob, target) = self
+            .prob_target_cache
+            .as_ref()
+            .expect("You have to call forward before calling backward!");
+        let n_obs = target.len();
+
+        -1.0 as Number / n_obs as Number * (target - prob)
     }
 }
