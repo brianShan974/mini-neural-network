66d2ad376c5f2b60b556fbd64826f631095aa063
单子亨 <2366910832@qq.com>
2025-03-11 20:02:56 +0000

    Implemented shuffle for Trainer

--- src/trainer.rs
+++ src/trainer.rs
@@ -1,3 +1,6 @@
+use ndarray::Axis;
+use rand::{rng, seq::SliceRandom};
+
 use crate::{
     Matrix, Number,
     layers::{layer::Layer, loss_layer::LossLayer},
@@ -49,6 +52,18 @@ impl Trainer {
     }
 
     fn shuffle(&mut self, input_dataset: Matrix, output_dataset: Matrix) -> (Matrix, Matrix) {
-        unimplemented!()
+        assert_eq!(
+            input_dataset.shape()[0],
+            output_dataset.shape()[0],
+            "They must have the same amount of samples!"
+        );
+
+        let mut indices: Vec<usize> = (0..input_dataset.shape()[0]).collect();
+        indices.shuffle(&mut rng());
+
+        (
+            input_dataset.select(Axis(1), &indices),
+            output_dataset.select(Axis(1), &indices),
+        )
     }
 }
