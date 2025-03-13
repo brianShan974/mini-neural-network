4e7ee9ee43f27a86eed47e77e8bc7b64253985cd
单子亨 <2366910832@qq.com>
2025-03-13 17:12:52 +0000

    Added a code skeleton for regressor

--- src/regressor.rs
+++ src/regressor.rs
@@ -1,6 +1,62 @@
-use crate::{layers::loss_layers::loss_layer::LossLayer, multilayer_network::MultiLayerNetwork};
+use polars::frame::DataFrame;
+
+use crate::{
+    Matrix, Number,
+    layers::{layer::Layer, loss_layers::loss_layer::LossLayer},
+    multilayer_network::MultiLayerNetwork,
+    preprocessor::Preprocessor,
+};
 
 pub struct Regressor<'a> {
     network: &'a mut MultiLayerNetwork,
     loss_layer: Box<dyn LossLayer>,
+    batch_size: usize,
+    n_epoch: usize,
+    learning_rate: Number,
+    x_preprocessor: Option<Preprocessor>,
+    y_preprocessor: Option<Preprocessor>,
+}
+
+impl<'a> Regressor<'a> {
+    pub fn new(
+        network: &mut MultiLayerNetwork,
+        loss_layer: Box<dyn LossLayer>,
+        batch_size: usize,
+        n_epoch: usize,
+        learning_rate: Number,
+    ) -> Self {
+        unimplemented!()
+    }
+
+    pub fn fit(&mut self, x_train: DataFrame, y_train: DataFrame, shuffle: bool) {
+        unimplemented!()
+    }
+
+    pub fn predict(&mut self, x: DataFrame) -> Matrix {
+        unimplemented!()
+    }
+
+    pub fn predict_only(&self, x: DataFrame) -> Matrix {
+        unimplemented!()
+    }
+
+    pub fn score(&self, x: DataFrame, y: DataFrame) -> Number {
+        unimplemented!()
+    }
+
+    fn preprocess_training(&mut self, x: DataFrame, y: DataFrame) -> (Matrix, Matrix) {
+        unimplemented!()
+    }
+
+    fn preprocess_non_training(&self, x: DataFrame, y: DataFrame) -> (Matrix, Matrix) {
+        unimplemented!()
+    }
+
+    fn forward(&mut self, x: Matrix) -> Matrix {
+        unimplemented!()
+    }
+
+    fn eval_only(&self, x: Matrix) -> Matrix {
+        unimplemented!()
+    }
 }
