9a9784d7d6c3b0b2b95adfdd1f882a068f805bfb
单子亨 <2366910832@qq.com>
2025-03-13 00:10:52 +0000

    Preprocessor now takes Option<Vector> instead of Vector

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -3,24 +3,37 @@ use crate::{
     utils::{max_in_matrix, min_in_matrix, repeat},
 };
 
+#[derive(Default)]
 pub struct Preprocessor {
-    min: Vector,
-    max: Vector,
+    min: Option<Vector>,
+    max: Option<Vector>,
 }
 
 impl Preprocessor {
     pub fn new(dataset: Matrix) -> Self {
         Self {
-            min: min_in_matrix(&dataset),
-            max: max_in_matrix(&dataset),
+            min: Some(min_in_matrix(&dataset)),
+            max: Some(max_in_matrix(&dataset)),
         }
     }
 
     pub fn apply(&self, dataset: Matrix) -> Matrix {
         let n = dataset.nrows();
 
-        let min_matrix = repeat(&self.min, n);
-        let max_matrix = repeat(&self.max, n);
+        let min_matrix = repeat(
+            &self
+                .min
+                .as_ref()
+                .expect("You must initialise the preprocessor first!"),
+            n,
+        );
+        let max_matrix = repeat(
+            &self
+                .max
+                .as_ref()
+                .expect("You must initialise the preprocessor first!"),
+            n,
+        );
 
         (dataset - &min_matrix) / (max_matrix - min_matrix)
     }
@@ -28,8 +41,20 @@ impl Preprocessor {
     pub fn revert(&self, dataset: Matrix) -> Matrix {
         let n = dataset.nrows();
 
-        let min_matrix = repeat(&self.min, n);
-        let max_matrix = repeat(&self.max, n);
+        let min_matrix = repeat(
+            &self
+                .min
+                .as_ref()
+                .expect("You must initialise the preprocessor first!"),
+            n,
+        );
+        let max_matrix = repeat(
+            &self
+                .max
+                .as_ref()
+                .expect("You must initialise the preprocessor first!"),
+            n,
+        );
 
         dataset * (max_matrix - &min_matrix) * min_matrix
     }
