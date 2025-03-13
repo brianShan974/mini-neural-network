51a6d64e946c57f8deea9b30bfe5befb3d329cea
单子亨 <2366910832@qq.com>
2025-03-13 02:50:31 +0000

    Now stores Vector instead of Option<Vector>

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -5,35 +5,23 @@ use crate::{
 
 #[derive(Default)]
 pub struct Preprocessor {
-    min: Option<Vector>,
-    max: Option<Vector>,
+    min: Vector,
+    max: Vector,
 }
 
 impl Preprocessor {
     pub fn new(dataset: MatrixView) -> Self {
         Self {
-            min: Some(min_in_matrix(dataset)),
-            max: Some(max_in_matrix(dataset)),
+            min: min_in_matrix(dataset),
+            max: max_in_matrix(dataset),
         }
     }
 
     pub fn apply(&self, dataset: Matrix) -> Matrix {
         let n = dataset.nrows();
 
-        let min_matrix = repeat(
-            self.min
-                .as_ref()
-                .expect("You must initialise the preprocessor first!")
-                .view(),
-            n,
-        );
-        let max_matrix = repeat(
-            self.max
-                .as_ref()
-                .expect("You must initialise the preprocessor first!")
-                .view(),
-            n,
-        );
+        let min_matrix = repeat(self.min.view(), n);
+        let max_matrix = repeat(self.max.view(), n);
 
         (dataset - &min_matrix) / (max_matrix - min_matrix)
     }
@@ -41,20 +29,8 @@ impl Preprocessor {
     pub fn revert(&self, dataset: Matrix) -> Matrix {
         let n = dataset.nrows();
 
-        let min_matrix = repeat(
-            self.min
-                .as_ref()
-                .expect("You must initialise the preprocessor first!")
-                .view(),
-            n,
-        );
-        let max_matrix = repeat(
-            self.max
-                .as_ref()
-                .expect("You must initialise the preprocessor first!")
-                .view(),
-            n,
-        );
+        let min_matrix = repeat(self.min.view(), n);
+        let max_matrix = repeat(self.max.view(), n);
 
         dataset * (max_matrix - &min_matrix) * min_matrix
     }
